31216c44f95a55c22f671b5a7137dbc86fed0774
einghstein <zdeninekf@seznam.cz>
2025-09-15 13:04:35 +0200

    updated mid.cmd

--- git-script/mid.cmd
+++ git-script/mid.cmd
@@ -1,2 +1,9 @@
+@echo off
+if "%~1"=="" (
+    echo You must provide a commit message.
+    echo Usage: mid "your commit message"
+    exit /b 1
+)
+
 git add .
-git commit -m "added git scripts"
+git commit -m "%*"
