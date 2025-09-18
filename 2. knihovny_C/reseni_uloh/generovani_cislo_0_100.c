d64f83af93ebc0bb300c7d914ac526d0876538c7
einghstein <zdeninekf@seznam.cz>
2025-09-18 10:57:30 +0200

    kostky

--- 2. knihovny_C/reseni_uloh/generovani_cislo_0_100.c
+++ 2. knihovny_C/reseni_uloh/generovani_cislo_0_100.c
@@ -2,11 +2,30 @@
 #include <stdlib.h>   
 #include <time.h>    
 
-int main() {
+int rand_in_range(int min, int max){
     srand(time(NULL));
+    int cislo = rand() % (max);
+    return cislo + min;
+}
 
-    int cislo = rand() % 100;
+int main() {
+    printf("%d\n\n",rand_in_range(1,2));
+    
+    int pocet_kostek;
+    int pocet_sten;
 
-    printf("Náhodné číslo: %d\n", cislo);
+    printf("Zadejte pocet kostek: ");
+    scanf("%d", &pocet_kostek);
+
+    printf("Zadejte pocet sten: ");
+    scanf("%d", &pocet_sten);
+
+    int v = 0;
+    for (int i = 0; i < pocet_kostek; i++){
+        v += rand_in_range(1,pocet_sten);
+    }
+    
+    printf("hod: %d\n", v);
+
     return 0;
 }
